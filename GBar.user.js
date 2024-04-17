c8740aca5e44e4f65416d037c66397d76de06255
CallyHam <87724457+CallyHam@users.noreply.github.com>
2024-04-17 17:34:12 +0100

    increment version number

--- GBar.user.js
+++ GBar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GBar
 // @namespace    https://www.github.com/CallyHam
-// @version      1.0.0
+// @version      1.0.1
 // @description  Bring back the gbar.
 // @author       CallyHam
 // @match        *://*.google.com/*
